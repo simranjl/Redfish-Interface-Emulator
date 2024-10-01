e1a3a6c4aeed96d0ebd8c6a68b926bd916303c9a
Simran Lekhwani <simran.lekhwani@se.com>
2024-10-01 09:44:58 -0400

    added template files for drives, operating system, and storage

---
+++ api_emulator/redfish/templates/drives.py
@@ -0,0 +1,40 @@
+# Drives Template File
+
+from copy import deepcopy
+import strgen
+
+DRIVES_TEMPLATE = {
+    "@odata.id": "{rb}{suffix}/Storage/{storage_id}/Drives/{drive_id}",
+    "@odata.type": "#Drives.v1_1_0.Drives",
+    "@odata.context": "{rb}$metadata#Drives.Drives",
+    "Id": "{drive_id}",
+    "Name": "{drive_name}",
+    "Model": None,
+    "Status": {
+        "Health": "OK",
+        "State": "Enabled"
+    },
+    "SerialNumber": None,
+    "CapacityBytes": "{bytes}"
+}
+
+
+def format_drives_template(**kwargs):
+    defaults = {
+        'rb': '/redfish/v1/',
+        'suffix': 'Systems',
+        'drive_model': strgen.StringGenerator('[A-Z]{1}[0-3]{3}').render(),
+        'serial_number': strgen.StringGenerator('[0-9]{10}').render()
+    }
+    defaults.update(kwargs)
+
+    c = deepcopy(DRIVES_TEMPLATE)
+    c['@odata.id'] = c['@odata.id'].format(**defaults)
+    c['@odata.context'] = c['@odata.context'].format(**defaults)
+    c['Id'] = c['Id'].format(**defaults)
+    c['Name'] = c['Name'].format(**defaults)
+    c['Model'] = defaults['Model']
+    c['SerialNumber'] = defaults['serial_number'],
+    c['CapacityBytes'] = c['CapacityBytes'].format(**defaults)
+
+    return c
---
+++ api_emulator/redfish/templates/operatingsystem.py
@@ -0,0 +1,42 @@
+from copy import deepcopy
+
+OPERATING_SYSTEM_TEMPLATE = {
+    "@odata.context": "{rb}$metadata#OperatingSystem.OperatingSystem",
+    "@odata.id": "{rb}{suffix}/{suffix_id}/OperatingSystem",
+    "@odata.type": "#OperatingSystem.v1_1_0.OperatingSystem",
+    "Name": "Computer Operating System",
+    "Id": "OperatingSystem",
+    "Type": "{type}",
+    "Description": "Operating System of the current computer system",
+    "Kernel": {
+        "Name": "{kernel_type}",
+        "Version": "{kernel_ver}"
+    },
+    "Status": {
+        "State": "Enabled",
+        "Health": "OK"
+    },
+}
+
+def format_operating_system_template(**kwargs) {
+    """
+    Formats the operating system template -- returns the template
+    """
+    # default params
+    defaults = {
+        'rb': '/redfish/v1/',
+        'suffix': 'Systems',
+        'type': 'Linux',
+        'kernel_type': 'Linux Kernel'
+    }
+    defaults.update(kwargs)
+
+    c = deepcopy(OPERATING_SYSTEM_TEMPLATE)
+    c['@odata.context'] = c['@odata.context'].format(**defaults)
+    c['@odata.id'] = c['@odata.id'].format(**defaults)
+    c['Type'] = c['Type'].format(**defaults)
+    c['Kernel']['Name'] = c['Kernel']['Name'].format(**defaults)
+    c['Kernal']['Version'] = c['Kernel']['Version'].format(**defaults)
+
+    return c 
+}
